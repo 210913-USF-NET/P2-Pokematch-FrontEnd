3b5929a6545727f615056078de3e90da6c14739f
berry-peachy <karla.rodriguez@revature.net>
2021-10-20 17:46:00 -0400

    navbar component

--- Pokematch-Front/src/app/app.module.ts
+++ Pokematch-Front/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
