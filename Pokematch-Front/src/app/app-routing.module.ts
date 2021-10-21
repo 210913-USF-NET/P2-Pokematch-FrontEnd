0e59392587abd4149ed4f2ad2c39fe57dfe9c574
Larry Moua <lkmoua@gmail.com>
2021-10-21 19:34:22 -0400

    routing ez

--- Pokematch-Front/src/app/app-routing.module.ts
+++ Pokematch-Front/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ElementListComponent } from './element-list/element-list.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'elements',
+    component: ElementListComponent
+  },
+  {
+    path: '',
+    component: HomePageComponent
+  }
+];
 
 @NgModule({
   declarations: [],
