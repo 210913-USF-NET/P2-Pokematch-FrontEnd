43dafbbd9a4e7c38d535ed92b67f801144ebb016
berry-peachy <karla.rodriguez@revature.net>
2021-10-21 18:55:13 -0400

    profile setup

--- Pokematch-Front/src/app/app-routing.module.ts
+++ Pokematch-Front/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'users/:id',
+  component: UserProfileComponent
+},
+
+{
+  path: 'users',
+  component: UserListComponent
+}
+
+
+];
 
 @NgModule({
   declarations: [],
--- Pokematch-Front/src/app/user-profile/user-profile.component.ts
+++ Pokematch-Front/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    
+  ) { }
 
   ngOnInit(): void {
   }
