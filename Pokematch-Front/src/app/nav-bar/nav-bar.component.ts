07548a04e0129370b8e0a3e6052dbcdf9099c77d
Jean Duong <bduong@mymail.mines.edu>
2021-10-26 16:05:02 -0600

    redirection works

--- Pokematch-Front/src/app/nav-bar/nav-bar.component.ts
+++ Pokematch-Front/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { PokeApiService } from '../service/poke-api.service';
 
 @Component({
   selector: 'nav-bar',
@@ -9,18 +10,21 @@ import { AuthService } from '@auth0/auth0-angular';
 })
 export class NavBarComponent implements OnInit {
 
+
   constructor(public auth: AuthService,
-    @Inject(DOCUMENT) private doc: Document) { }
+    @Inject(DOCUMENT) private doc: Document, private pokeService: PokeApiService) { }
 
   ngOnInit(): void {
+
   }
 
-  loginWithRedirect(): void {
-    this.auth.loginWithRedirect({ appState: { target: '/userprofile'} });
+  login(): void {
+
+    this.auth.loginWithRedirect({ appState: { target: '/elements' } });
+
   }
 
   logout(): void {
     this.auth.logout({ returnTo: this.doc.location.origin });
   }
-
 }
