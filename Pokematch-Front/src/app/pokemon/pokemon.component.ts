11976194e2a628fab4bcb700831d183cd4ea4555
Jean Duong <bduong@mymail.mines.edu>
2021-10-26 16:16:08 -0600

    added another pokemon

--- Pokematch-Front/src/app/pokemon/pokemon.component.ts
+++ Pokematch-Front/src/app/pokemon/pokemon.component.ts
@@ -4,7 +4,7 @@ import { pokemon } from '../../app/models/pokemon';
 var pokelist: pokemon[] = [];
 var pokenames: pokemon[] = [];
 var poketype: pokemon[] = [];
-var profilepic: pokemon[] = []; 
+var profilepic: pokemon[] = [];
 var favoritepokemon: pokemon[] = [];
 @Component({
   selector: 'app-pokemon',
@@ -14,79 +14,65 @@ var favoritepokemon: pokemon[] = [];
 export class PokemonComponent implements OnInit {
 
   constructor(private route: Router) { }
-  
+
   ngOnInit() {
-    for(let i = 0; i <= 9; i++)
-{
-  this.getPokemon(i);
-  if(i == 9)
-  {
-    break;
+    for (let i = 0; i < 11; i++) {
+      this.getPokemon(i);
+    }
   }
-}
 
-  
-}
-  
 
-    getPokemon(name: number){
-      let pokeUrl = 'https://pokeapi.co/api/v2/pokemon/' + name;
+  getPokemon(name: number) {
+    let pokeUrl = 'https://pokeapi.co/api/v2/pokemon/' + name;
 
-      let xhr = new XMLHttpRequest();
-      xhr.open("GET", pokeUrl, true);
-      xhr.send();
-      let pokemon: any = {}
-      xhr.onreadystatechange = function(){
-        if (this.readyState == 4)
-        {
-          if(this.status > 199 && this.status < 300)
-          {
-            pokemon = JSON.parse(xhr.responseText);
-            console.log(pokemon);
-            let div = document.querySelector('.pokemon');
-            document.querySelectorAll('.pokemon caption').forEach((el) => el.remove());
-            let captionEl = document.createElement('caption');
-            let pokename = document.createTextNode(pokemon.name);
-            captionEl.appendChild(pokename);
-            // div.appendChild(captionEl);
-            pokelist.push(pokemon.sprites.front_default);
-            pokenames.push(pokemon.name);
-            poketype.push(pokemon.types[0].type.name)
-          }
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", pokeUrl, true);
+    xhr.send();
+    let pokemon: any = {}
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status > 199 && this.status < 300) {
+          pokemon = JSON.parse(xhr.responseText);
+          console.log(pokemon);
+          let div = document.querySelector('.pokemon');
+          document.querySelectorAll('.pokemon caption').forEach((el) => el.remove());
+          let captionEl = document.createElement('caption');
+          let pokename = document.createTextNode(pokemon.name);
+          captionEl.appendChild(pokename);
+          // div.appendChild(captionEl);
+          pokelist.push(pokemon.sprites.front_default);
+          pokenames.push(pokemon.name);
+          poketype.push(pokemon.types[0].type.name)
         }
       }
     }
+  }
 
-    getPokemonList()
-    {
-      // document.getElementById("List").innerHTML = '<img src="' + pokelist.join('" /><img src="') + '" />';
-      // document.getElementById("names").innerHTML = '   ' + pokenames.join('   ') + '   ';
-      for(let i = 1; i <= 9; i++)
-      {
-        document.getElementById(String(i)).innerHTML = '<img src="' + pokelist[i-1] + ('" /><img src="') + '" />' + 'Pokemon: ' + pokenames[i-1] + ', ' + 'Type: ' + poketype[i-1];
-      }
+  getPokemonList() {
+    // document.getElementById("List").innerHTML = '<img src="' + pokelist.join('" /><img src="') + '" />';
+    // document.getElementById("names").innerHTML = '   ' + pokenames.join('   ') + '   ';
+    for (let i = 1; i < 11; i++) {
+      document.getElementById(String(i)).innerHTML = '<img src="' + pokelist[i - 1] + ('" /><img src="') + '" />' + 'Pokemon: ' + pokenames[i - 1] + ', ' + 'Type: ' + poketype[i - 1];
     }
+  }
 
-    selectedPokemon(name: number)
-    {
-      if(profilepic[0] == null)
-      {
+  selectedPokemon(name: number) {
+    if (profilepic[0] == null) {
       alert("You have chosen " + pokenames[name - 1] + " for your profile picture!");
-      profilepic.push(pokelist[name-1]);
-      document.getElementById('profilepic').innerHTML =  '<img src="' + pokelist[name-1] + ('" /><img src="') + 'width = "50"' + 'height="50"'
+      profilepic.push(pokelist[name - 1]);
+      document.getElementById('profilepic').innerHTML = '<img src="' + pokelist[name - 1] + ('" /><img src="') + 'width = "50"' + 'height="50"'
       document.getElementById('directions').innerHTML = "Please select your top 3 favortite pokemon! The first selection being your favorite and the third selection being your 3rd favorite."
       return;
-      } 
-      if(favoritepokemon[2] != null)
-      {
-        alert("You may only select 3 favorite pokemon");
-        console.log(favoritepokemon);
-        console.log(profilepic);
-        return;
-      }
-      alert("You have selected " + pokenames[name -1])
-      favoritepokemon.push(pokenames[name-1]);
-      return;
-      // this.route.navigate(['/pokemonselection'])
     }
+    if (favoritepokemon[2] != null) {
+      alert("You may only select 3 favorite pokemon");
+      console.log(favoritepokemon);
+      console.log(profilepic);
+      return;
+    }
+    alert("You have selected " + pokenames[name - 1])
+    favoritepokemon.push(pokenames[name - 1]);
+    return;
+    // this.route.navigate(['/pokemonselection'])
+  }
 }
