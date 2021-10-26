7eda7b0e7c53040d28f946b913a9fdcc241be008
Jean Duong <bduong@mymail.mines.edu>
2021-10-26 15:03:38 -0600

    pull

--- Pokematch-Front/src/app/nav-bar/nav-bar.component.ts
+++ Pokematch-Front/src/app/nav-bar/nav-bar.component.ts
@@ -1,20 +1,20 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
-import { User } from '@auth0/auth0-spa-js';
 
 @Component({
-selector: 'nav-bar',
-templateUrl: './nav-bar.component.html',
-styleUrls: ['./nav-bar.component.css']
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
 
-constructor(public auth: AuthService,
-  @Inject(DOCUMENT) private doc: Document) { }
+  constructor(public auth: AuthService,
+    @Inject(DOCUMENT) private doc: Document) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void {
+  }
+
   loginWithRedirect(): void {
     this.auth.loginWithRedirect();
   }
