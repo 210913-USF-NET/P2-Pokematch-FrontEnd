9abacb96e60cfd9977dd8bc42b2038163e8a3b9d
berry-peachy <karla.rodriguez@revature.net>
2021-10-27 22:53:03 -0400

    setting up tests

--- Pokematch-Front/src/app/Service/poke-api.service.spec.ts
+++ Pokematch-Front/src/app/Service/poke-api.service.spec.ts
@@ -1,16 +1,22 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
-import { PokeApiService } from './poke-api.service';
+import { PokeApiService } from  '../service/poke-api.service';
 
 describe('PokeApiService', () => {
   let service: PokeApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(PokeApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  
 });
